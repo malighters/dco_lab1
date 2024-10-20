f7283d27c3386fb75791db7e87e032911624a27b
Yevhenii Chernenko <chernenko.yevheniy@gmail.com>
2024-10-21 00:40:49 +0200

    Recursive approach with immutable collections returned

--- src/main/scala/LabApplication.scala
+++ src/main/scala/LabApplication.scala
@@ -1,8 +1,6 @@
 import java.lang.Runtime
-import java.util.concurrent.ForkJoinPool
+import java.util.concurrent.{ForkJoinPool, RecursiveTask}
 package de.othr.dco {
-
-  import java.util.concurrent.RecursiveAction
 
   object LabApplication extends App {
 
@@ -13,47 +11,64 @@ package de.othr.dco {
    */
     val start = 0
     val end = 24
-    val inclusiveRange: Array[Int] = (start to end).toArray
+    val inclusiveRange: List[Int] = (start to end).toList
     println(inclusiveRange.mkString("Array(", ", ", ")"))
 
     val numberOfProcessors = Runtime.getRuntime.availableProcessors()
 
-    val pool: ForkJoinPool = new ForkJoinPool(4)
+    val pool: ForkJoinPool = new ForkJoinPool(numberOfProcessors)
 
-    val recursiveTask = new RecursiveMutable(inclusiveRange, start, end)
+    val recursiveTask = new RecursiveImmutable(inclusiveRange)
 
     val startTime = System.currentTimeMillis()
 
     // Execute the task
-    val result = pool.invoke(recursiveTask)
+    val result = pool.submit(recursiveTask)
 
     val endTime = System.currentTimeMillis()
 
     println(endTime - startTime)
 
-    println(inclusiveRange.mkString("Array(", ", ", ")"))
+    println(result.get().mkString("Array(", ", ", ")"))
     pool.shutdown()
 
   }
 
-  class RecursiveMutable(array: Array[Int], start: Int, end: Int) extends RecursiveAction {
-    override def compute(): Unit = {
-      if (end - start <= 10) {
-        for (i <- start to end) {
-          array(i) = array(i) * 2
-        }
-      }
-      else {
-        val mid = (end + start) / 2
-        val left = new RecursiveMutable(array, start, mid)
-        val right = new RecursiveMutable(array, mid, end)
-        left.fork()
-        right.compute()
+//  class RecursiveMutable(array: Array[Int], start: Int, end: Int) extends RecursiveAction {
+//    override def compute(): Unit = {
+//      if (end - start <= 10) {
+//        for (i <- start to end) {
+//          array(i) = array(i) * 2
+//        }
+//      }
+//      else {
+//        val mid = (end + start) / 2
+//        val left = new RecursiveMutable(array, start, mid)
+//        val right = new RecursiveMutable(array, mid, end)
+//        left.fork()
+//        right.compute()
+//
+//        left.join()
+//      }
+//    }
+//  }
 
-        left.join()
+  class RecursiveImmutable(list: List[Int]) extends RecursiveTask[List[Int]] {
+    override def compute(): List[Int] = {
+      if(list.length <= 10)
+        list.map(i => i * 2)
+      else {
+        val (leftList, rightList) = list.splitAt(list.length / 2)
+        val left = new RecursiveImmutable(leftList)
+        val right = new RecursiveImmutable(rightList)
+        left.fork()
+        right.fork()
+
+        val leftResult = left.join()
+        val rightResult = right.join()
+        leftResult ++ rightResult
       }
     }
-
   }
 
 }
